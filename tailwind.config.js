1fa2e203ed522ded5ec7dd75684f0d616a674710
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2024-11-18 09:49:31 +0500

    change

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+    extend: {
+      backgroundImage: {
+        work: "url('/image/work.png')", 
+      },
+    },
+  },
+  plugins: [],
+};
